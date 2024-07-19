1abe316593b7f0e257365823a5d6c46977f2fa4b
getwlad <wladmir.rrodrigues@gmail.com>
2024-07-18 21:52:02 -0300

    feat: evento para aparecer prompt

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const exibirAlerta = (event) => {
   alert(`Você clicou! coordenadas: X: ${event.clientX}, Y: ${event.clientY}`);
 };
+
+const alterarTexto = () => {
+  const titulo = document.getElementById("titulo");
+  const texto = prompt("Digite o seu texto");
+  titulo.innerText = texto;
+};
